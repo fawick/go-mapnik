8eacf9a387afc861e84d6b5620ea29c26770c9c4
Fabian Wickborn <fabian@wickborn.net>
2013-12-11 19:27:00 +0100

    example.go: Render Europe in EPSG:4326 to memory and save the blob

--- example/example.go
+++ example/example.go
@@ -1,14 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"github.com/fawick/go-mapnik/mapnik"
+	"io/ioutil"
 )
 
 func main() {
-	mapnik.RegisterDatasources("/usr/lib/mapnik/input")
 	m := mapnik.NewMap(1600, 1200)
 	defer m.Free()
 	m.Load("sample/stylesheet.xml")
-	m.ZoomAll()
-	m.RenderToFile("mapnik.png")
+	fmt.Println(m.SRS())
+	m.ZoomToMinMax(0, 35, 16, 70)
+	ioutil.WriteFile("mapnik.png", m.RenderToMemoryPng(), 0644)
 }
