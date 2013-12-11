652e2dc178ac7bbb285340213bbdd19b0f1faea8
Fabian Wickborn <fabian@wickborn.net>
2013-12-11 19:27:36 +0100

    Rendering to memory, zooming to bounding box and autoload of fonts

--- mapnik/mapnik.go
+++ mapnik/mapnik.go
@@ -3,16 +3,30 @@ package mapnik
 // #include "mapnik_c_api.h"
 import "C"
 
-// TODO func init() for running RegisterDatasources (like in the python bindings)
+import (
+	"unsafe"
+)
+
+func init() {
+	// register default datasources path and fonts path like the python bindings do
+	RegisterDatasources(pluginPath)
+	RegisterFonts(fontPath)
+}
 
 func RegisterDatasources(path string) {
 	C.mapnik_register_datasources(C.CString(path))
 }
 
+func RegisterFonts(path string) {
+	C.mapnik_register_fonts(C.CString(path))
+}
+
+// Point in 2D space
 type Coord struct {
 	X, Y float64
 }
 
+// Projection from one reference system to the other
 type Projection struct {
 	p *C.struct_mapnik_projection_t
 }
@@ -28,14 +42,12 @@ func (p Projection) Forward(coord Coord) Coord {
 	return Coord{float64(c.x), float64(c.y)}
 }
 
+// Map base type
 type Map struct {
 	m *C.struct_mapnik_map_t
 }
 
 func NewMap(width, height uint32) *Map {
-	//m := new(Map)
-	//m.m = C.mapnik_map(C.uint(width), C.uint(height))
-	//return m
 	return &Map{C.mapnik_map(C.uint(width), C.uint(height))}
 }
 
@@ -64,6 +76,20 @@ func (m *Map) ZoomAll() {
 	C.mapnik_map_zoom_all(m.m)
 }
 
+func (m *Map) ZoomToMinMax(minx, miny, maxx, maxy float64) {
+	bbox := C.mapnik_bbox(C.double(minx), C.double(miny), C.double(maxx), C.double(maxy))
+	defer C.mapnik_bbox_free(bbox)
+	C.mapnik_map_zoom_to_box(m.m, bbox)
+}
+
 func (m *Map) RenderToFile(path string) {
 	C.mapnik_map_render_to_file(m.m, C.CString(path))
+}
+
+func (m *Map) RenderToMemoryPng() []byte {
+	i := C.mapnik_map_render_to_image(m.m)
+	defer C.mapnik_image_free(i)
+	b := C.mapnik_image_to_png_blob(i)
+	defer C.mapnik_image_blob_free(b)
+	return C.GoBytes(unsafe.Pointer(b.ptr), C.int(b.len))
 }
