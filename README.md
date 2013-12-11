40a9e728226eedc7204d40f41d2b5f5ee4a6b485
Fabian Wickborn <fabian@wickborn.net>
2013-12-11 15:44:14 +0100

    Add configuration script for Windows builds

--- README.md
+++ README.md
@@ -15,11 +15,11 @@ Installation
 1. Install Mapnik build environmnent,
 	- e.g. Ubuntu: `apt-get install libmapnik-dev`
 2. Download the sources, either by 
-- `git clone` the repository to $GOPATH/src/fawick/go-mapnik 
+    - `git clone` the repository to $GOPATH/src/fawick/go-mapnik 
 
 	OR
 
-- `go get -d github.com/fawick/go-mapnik`
+    - `go get -d github.com/fawick/go-mapnik/mapnik`
 3. `cd mapnik` and run the configuration script `./configure.bash`. 
    That script will setup the correct paths for including Mapnik headers and
    linking against the Mapnik shared library, as well as download the Mapnik C
@@ -32,7 +32,9 @@ Installation
 
 On Windows, go-mapnik is restricted to GOARCH=386 for the moment, as the SDK is
 build with MSVC 32-bit and the C api must be build with a compatible
-compiler, before the Go bindings can be linked against them.
+compiler as a DLL before the Go bindings can be linked against it. This way
+
+You also need to have the MSVC 10 compiler installed on your system.
 
 So, make sure `go version` reports `windows/386` as your toolchain. If you
 happen to have the `windows/amd64` toolchain, either download Go binaries for
@@ -42,17 +44,17 @@ binaries on your own.
 
 1. Install Mapnik Windows 32bit SDK from http://www.mapnik.org/download
 2. Download the sources, either by 
-- `git clone` the repository to $GOPATH/src/fawick/go-mapnik 
-
-	OR
-
-- `go get -d github.com/fawick/go-mapnik`
-3. Open a MSVC command prompt and run `configure.bat` to compile a C library
+    + `git clone` the repository to $GOPATH/src/fawick/go-mapnik 
+	
+    OR
+	
+    + `go get -d github.com/fawick/go-mapnik/mapnik`
+3. Run `configure.cmd` in the folder `mapnik` to compile a C DLL
    that can be used by Go/CGO/GCC later (sources will be downloaded
    automatically)
 4. `go install` the bindings in `./mapnik` or `cd example && go run example.go`
-    (Make sure your %PATH% environment variable contains the path of
-    `mapnik.dll`.)
+    (Make sure your %PATH% environment variable contains the paths of both
+    `mapnik.dll` and the newly created `mapnik_c_api.dll`.)
 
 Usage
 -----
---
+++ mapnik/configure.cmd
@@ -0,0 +1,63 @@
+cls
+
+setlocal EnableDelayedExpansion
+
+if not exist mapnik_c_api.cpp curl -o mapnik_c_api.cpp https://raw.github.com/fawick/mapnik-c-api/master/mapnik_c_api.c
+if not exist mapnik_c_api.h curl -O https://raw.github.com/fawick/mapnik-c-api/master/mapnik_c_api.h
+
+If DEFINED ProgramFiles(x86) Set BUILDTOOLS32BIT=%ProgramFiles(x86)%
+If NOT DEFINED ProgramFiles(x86) Set BUILDTOOLS32BIT=%ProgramFiles%
+
+call "%BUILDTOOLS32BIT%\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86
+
+
+set MAPNIK_C_DEFINES=-DUNICODE -DWIN32
+
+set PATH=%PATH%;c:\mapnik-v2.2.0\bin
+
+for /f "delims=" %%i in ('mapnik-config --prefix') do set MAPNIK_PATH=%%i
+for /f "delims=" %%i in ('mapnik-config --cxxflags') do set MAPNIK_CXXFLAGS=%%i
+for /f "delims=" %%i in ('mapnik-config --ldflags') do set MAPNIK_LDFLAGS=%%i
+for /f "delims=" %%i in ('mapnik-config --dep-libs') do set MAPNIK_DEPLIBS=%%i
+for /f "delims=" %%i in ('mapnik-config --libs') do set MAPNIK_LIBS=%%i
+for /f "delims=" %%i in ('mapnik-config --defines') do set MAPNIK_DEFINES_LIST=%%i
+for /f "delims=" %%i in ('mapnik-config --fonts') do set MAPNIK_FONTS_DIRECTORY=%%i
+for /f "delims=" %%i in ('mapnik-config --input-plugins') do set MAPNIK_INPUT_PLUGINS_DIRECTORY=%%i
+
+
+call :constructDefines %MAPNIK_DEFINES_LIST%
+
+echo.LDFLAGS=%MAPNIK_LDFLAGS%
+echo.LIBS=%MAPNIK_LIBS%
+echo.DEFINES=%MAPNIK_C_DEFINES%
+echo.FONTS=%MAPNIK_FONTS_DIRECTORY%
+echo.PLUGINS=%MAPNIK_INPUT_PLUGINS_DIRECTORY%
+
+
+> gen_import_windows.go (
+    echo.package mapnik
+	echo.
+    echo.// #cgo LDFLAGS: mapnik_c_api.dll
+	echo.import"C"
+	echo.
+	echo.const (
+	echo.    fontPath = "%MAPNIK_FONTS_DIRECTORY%"
+	echo.    pluginPath = "%MAPNIK_INPUT_PLUGINS_DIRECTORY%"
+	echo.^)
+)
+
+if not exist mapnik_c_api.obj cl -c -nologo -Zm200 -Zc:wchar_t- -O2 %MAPNIK_CXXFLAGS% -W3 -w34100 -w34189 %MAPNIK_C_DEFINES% -I"c:\mapnik-v2.2.0\include" -I"c:\mapnik-v2.2.0\include\mapnik\agg" -I"." mapnik_c_api.cpp
+::link /LIBPATH:%MAPNIK_LDFLAGS% %MAPNIK_LIBS% %MAPNIK_DEPLIBS% mapnik_c_api.obj /NOLOGO /DYNAMICBASE /NXCOMPAT /INCREMENTAL:NO /DLL /OUT:mapnik_c_api.dll 
+link /LIBPATH:%MAPNIK_LDFLAGS% %MAPNIK_LIBS%  mapnik_c_api.obj /DLL /OUT:mapnik_c_api.dll 
+
+del mapnik_c_api.obj  mapnik_c_api.cpp  mapnik_c_api.lib  mapnik_c_api.exp
+
+goto :eof
+
+:constructDefines
+	if "%1"=="" exit /b
+	set MAPNIK_C_DEFINES=%MAPNIK_C_DEFINES% -D%1
+	shift
+	goto :constructDefines
+
+
