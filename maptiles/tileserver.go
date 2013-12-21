55dd75144bff4fc0fb247da3ea6310862655960b
Fabian Wickborn <fabian@wickborn.net>
2013-12-21 23:59:09 +0100

    Refactored tile serving methods

--- maptiles/tileserver.go
+++ maptiles/tileserver.go
@@ -31,6 +31,37 @@ func (t *TileServer) AddMapnikLayer(layerName string, stylesheet string) {
 
 var pathRegex = regexp.MustCompile(`/([A-Za-z0-9]+)/([0-9]+)/([0-9]+)/([0-9]+)\.png`)
 
+func (t *TileServer) ServeTileRequest(w http.ResponseWriter, r *http.Request, tc TileCoord) {
+	ch := make(chan TileFetchResult)
+
+	tr := TileFetchRequest{tc, ch}
+	t.m.RequestQueue() <- tr
+
+	result := <-ch
+	needsInsert := false
+
+	if result.BlobPNG == nil {
+		// Tile was not provided by DB, so submit the tile request to the renderer
+		t.lmp.SubmitRequest(tr)
+		result = <-ch
+		if result.BlobPNG == nil {
+			// The tile could not be rendered, now we need to bail out.
+			http.NotFound(w, r)
+			return
+		}
+		needsInsert = true
+	}
+
+	w.Header().Set("Content-Type", "image/png")
+	_, err := w.Write(result.BlobPNG)
+	if err != nil {
+		log.Println(err)
+	}
+	if needsInsert {
+		t.m.InsertQueue() <- result // insert newly rendered tile into cache db
+	}
+}
+
 func (t *TileServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	path := pathRegex.FindStringSubmatch(r.URL.Path)
 
@@ -43,35 +74,6 @@ func (t *TileServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	z, _ := strconv.ParseUint(path[2], 10, 64)
 	x, _ := strconv.ParseUint(path[3], 10, 64)
 	y, _ := strconv.ParseUint(path[4], 10, 64)
-	ch := make(chan TileFetchResult)
 
-	request := TileFetchRequest{TileCoord{x, y, z, t.TmsSchema, l}, ch}
-	t.m.RequestQueue() <- request
-
-	result := <-ch
-	needsInsert := false
-
-	if result.BlobPNG == nil {
-		// Tile was not provided by DB, so submit the tile request to the renderer
-		log.Println("tile cache miss", z, x, y)
-		t.lmp.SubmitRequest(request)
-		result = <-ch
-		if result.BlobPNG == nil {
-			// The tile could not be rendered, now we need to bail out.
-			http.NotFound(w, r)
-			return
-		}
-		needsInsert = true
-	} else {
-		log.Println("tile cache hit", z, x, y)
-	}
-
-	w.Header().Set("Content-Type", "image/png")
-	_, err := w.Write(result.BlobPNG)
-	if err != nil {
-		log.Println(err)
-	}
-	if needsInsert {
-		t.m.InsertQueue() <- result // insert newly rendered tile into cache db
-	}
+	t.ServeTileRequest(w, r, TileCoord{x, y, z, t.TmsSchema, l})
 }
